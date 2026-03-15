4484786a0bb73ebb07881fdb96bee547aa7a04f6
Кротова Алёна <61360466+lol50@users.noreply.github.com>
2026-03-15 21:18:39 +0300

    Add files via upload

---
+++ лр16/4 (stek)/stek.pas
@@ -0,0 +1,68 @@
+﻿type PNode = ^Node;
+     Node = record
+       data: integer;
+       next: PNode;
+     end;
+
+procedure Push(var Head: PNode; x: integer);
+var NewNode: PNode;
+begin
+  New(NewNode);
+  NewNode^.data := x;
+  NewNode^.next := Head;
+  Head := NewNode;
+end;
+
+function Pop(var Head: PNode): integer;
+var q: PNode;
+begin
+  if Head = nil then
+  begin
+    writeln('Ошибка: стек пуст!');
+    Result := 0;
+    Exit;
+  end;
+  
+  Result := Head^.data;
+  q := Head;
+  Head := Head^.next;
+  Dispose(q);
+end;
+
+function IsEmpty(Head: PNode): boolean;
+begin
+  Result := (Head = nil);
+end;
+
+var InputFile, OutputFile: Text;
+    StackHead: PNode;
+    num: integer;
+begin
+  StackHead := nil;
+  assign(InputFile, 'input.txt');
+  reset(InputFile);
+  writeln('Читаем числа из файла input.txt:');
+  while not eof(InputFile) do
+  begin
+    read(InputFile, num);
+    write(num, ' ');
+    Push(StackHead, num);
+  end;
+  writeln;
+  close(InputFile);
+
+  assign(OutputFile, 'output.txt');
+  rewrite(OutputFile);
+  writeln('Записываем числа в обратном порядке в файл output.txt:');
+  while not IsEmpty(StackHead) do
+  begin
+    num := Pop(StackHead);
+    write(num, ' ');
+    write(OutputFile, num, ' ');
+  end;
+  writeln;
+  close(OutputFile);
+  
+  while not IsEmpty(StackHead) do
+    Pop(StackHead);
+end.
